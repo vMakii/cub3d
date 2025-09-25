ad7602c2531a6fa522383a9129823e6bad3d245e
Mika Vogel <mivogel@student.42.fr>
2025-09-25 14:37:16 +0200

    update

--- mandatory/srcs/utils/fps_counter_bonus.c
+++ mandatory/srcs/utils/fps_counter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 12:54:24 by mivogel           #+#    #+#             */
-/*   Updated: 2025/09/23 11:14:11 by mivogel          ###   ########.fr       */
+/*   Updated: 2025/09/25 14:27:24 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,79 +29,84 @@ float	get_time(void)
 		/ 1000000.0);
 }
 
-static void	update_frame_times(t_fps *fps_data, float frame_time)
-{
-	if (fps_data->frames_counted < 60)
-	{
-		fps_data->frame_times[fps_data->frame_index] = frame_time;
-		fps_data->total_time += frame_time;
-		fps_data->frames_counted++;
-	}
-	else
-	{
-		fps_data->total_time -= fps_data->frame_times[fps_data->frame_index];
-		fps_data->frame_times[fps_data->frame_index] = frame_time;
-		fps_data->total_time += frame_time;
-	}
-	fps_data->frame_index = (fps_data->frame_index + 1) % 60;
-}
+// static void	update_frame_times(t_fps *fps_data, float frame_time)
+// {
+// 	if (fps_data->frames_counted < 60)
+// 	{
+// 		fps_data->frame_times[fps_data->frame_index] = frame_time;
+// 		fps_data->total_time += frame_time;
+// 		fps_data->frames_counted++;
+// 	}
+// 	else
+// 	{
+// 		fps_data->total_time -= fps_data->frame_times[fps_data->frame_index];
+// 		fps_data->frame_times[fps_data->frame_index] = frame_time;
+// 		fps_data->total_time += frame_time;
+// 	}
+// 	fps_data->frame_index = (fps_data->frame_index + 1) % 60;
+// }
 
-static void	calculate_fps(t_data *data, t_fps *fps_data)
-{
-	float	avg_frame_time;
+// static void	calculate_fps(t_data *data, t_fps *fps_data)
+// {
+// 	float	avg_frame_time;
 
-	if (fps_data->frames_counted > 0)
-	{
-		avg_frame_time = fps_data->total_time / fps_data->frames_counted;
-		data->time.fps = (int)(1.0f / avg_frame_time);
-	}
-	else
-		data->time.fps = 60;
-	if (data->time.fps < 1)
-		data->time.fps = 1;
-	if (data->time.fps > 1000)
-		data->time.fps = 1000;
-}
+// 	if (fps_data->frames_counted > 0)
+// 	{
+// 		avg_frame_time = fps_data->total_time / fps_data->frames_counted;
+// 		data->time.fps = (int)(1.0f / avg_frame_time);
+// 	}
+// 	else
+// 		data->time.fps = 60;
+// 	if (data->time.fps < 1)
+// 		data->time.fps = 1;
+// 	if (data->time.fps > 1000)
+// 		data->time.fps = 1000;
+// }
 
-void	print_fps(t_data *data)
-{
-	int			current_fps;
-	static int	old_fps;
-	char		*fps;
+// void	print_fps(t_data *data)
+// {
+// 	int			current_fps;
+// 	static int	old_fps;
+// 	char		*fps;
 
-	current_fps = data->time.fps;
-	if (!old_fps)
-		old_fps = current_fps;
-	if (g_frame_count % 60 == 0)
-		old_fps = current_fps;
-	fps = ft_itoa(old_fps);
-	mlx_string_put(data->mlx.ptr, data->mlx.win, WIN_WIDTH - 60, 20,
-		COLOR_WHITE, "FPS: ");
-	mlx_string_put(data->mlx.ptr, data->mlx.win, WIN_WIDTH - 30, 20,
-		COLOR_WHITE, fps);
-	free(fps);
-}
+// 	current_fps = data->time.fps;
+// 	if (!old_fps)
+// 		old_fps = current_fps;
+// 	if (g_frame_count % 60 == 0)
+// 		old_fps = current_fps;
+// 	fps = ft_itoa(old_fps);
+// 	mlx_string_put(data->mlx.ptr, data->mlx.win, WIN_WIDTH - 60, 20,
+// 		COLOR_WHITE, "FPS: ");
+// 	mlx_string_put(data->mlx.ptr, data->mlx.win, WIN_WIDTH - 30, 20,
+// 		COLOR_WHITE, fps);
+// 	free(fps);
+// }
 
-void	fps_counter(t_data *data)
-{
-	t_fps	fps_data;
+// void	fps_counter(t_data *data)
+// {
+// 	static t_fps	fps_data;
+// 	static int		initialized = 0;
 
-	g_frame_count++;
-	memset(fps_data.frame_times, 0, sizeof(fps_data.frame_times));
-	fps_data.frame_index = 0;
-	fps_data.total_time = 0.0f;
-	fps_data.frames_counted = 0;
-	if (g_frame_count == 120)
-		g_frame_count = 0;
-	fps_data.current_time = get_time();
-	data->time.frametime = fps_data.current_time - data->time.oldtime;
-	data->time.oldtime = fps_data.current_time;
-	data->time.time = fps_data.current_time;
-	if (data->time.frametime <= 0.000001f || data->time.frametime > 1.0f)
-		data->time.frametime = 0.016667f;
-	update_frame_times(&fps_data, data->time.frametime);
-	calculate_fps(data, &fps_data);
-}
+// 	g_frame_count++;
+// 	if (!initialized)
+// 	{
+// 		memset(fps_data.frame_times, 0, sizeof(fps_data.frame_times));
+// 		fps_data.frame_index = 0;
+// 		fps_data.total_time = 0.0f;
+// 		fps_data.frames_counted = 0;
+// 		initialized = 1;
+// 	}
+// 	if (g_frame_count == 120)
+// 		g_frame_count = 0;
+// 	fps_data.current_time = get_time();
+// 	data->time.frametime = fps_data.current_time - data->time.oldtime;
+// 	data->time.oldtime = fps_data.current_time;
+// 	data->time.time = fps_data.current_time;
+// 	if (data->time.frametime <= 0.000001f || data->time.frametime > 1.0f)
+// 		data->time.frametime = 0.016667f;
+// 	update_frame_times(&fps_data, data->time.frametime);
+// 	calculate_fps(data, &fps_data);
+// }
 
 // Print FPS with more detailed info every 60 frames
 // if (g_frame_count % 60 == 0)
